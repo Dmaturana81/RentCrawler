6b902ea128500f40d5ea24d99632b4e4df620105
Dmaturana81 <elmaturana@gmail.com>
2023-11-14 09:56:53 -0300

    Updated to scrapy 2.11

--- deploy.py
+++ deploy.py
@@ -1,13 +1,16 @@
 from scrapy.utils.project import get_project_settings
 from scrapy.crawler import CrawlerProcess
+from scrapy.spiderloader import SpiderLoader
 
 setting = get_project_settings()
 process = CrawlerProcess(setting)
+spiderloaders = SpiderLoader(setting)
 
-for spider_name in process.spiders.list():
+for spider_name in spiderloaders.list():
     if spider_name == 'zap':
         continue
-    print ("Running spider %s" % (spider_name))
-    process.crawl(spider_name) #query dvh is custom argument used in your scrapy
+    print("Running spider %s" % (spider_name))
+    # query dvh is custom argument used in your scrapy
+    process.crawl(spider_name)
 
 process.start()
